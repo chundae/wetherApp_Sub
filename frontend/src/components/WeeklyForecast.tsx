6d26d2c4d84ade9f3dc03d047db7a2bbbcfba157
kiseok <kisunk4546@gmail.com>
2024-10-15 17:40:05 +0900

    Remove cache files from Git tracking

---
+++ frontend/src/components/WeeklyForecast.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const WeeklyForecast = () => {
+    return(
+        <div className="weekly-forecast">
+            <h2>aaaaaaaa</h2>
+        </div>
+    );
+};
+
+
+export default WeeklyForecast;
