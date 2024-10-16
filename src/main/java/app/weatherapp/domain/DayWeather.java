9ae1e5456de6a852004cc3794a6cb5483c51c915
kiseok <kisunk4546@gmail.com>
2024-10-16 23:11:45 +0900

    weather Tables/Entity create

--- src/main/java/app/weatherapp/domain/DayWeather.java
+++ src/main/java/app/weatherapp/domain/DayWeather.java
@@ -1,4 +1,35 @@
 package app.weatherapp.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Table(name = "short_Time_weather")
+@Entity
+@Getter
 public class DayWeather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ST_Id;
+
+    @JoinColumn(name = "location_id")
+    @ManyToOne
+    private Location location;
+
+    @Column(name = "temperature_hour")
+    private Double temperatureHour;
+
+    @Column(name = "min_temp")
+    private Double minTemp;
+
+    @Column(name = "max_temp")
+    private Double maxTemp;
+
+    @Column(name = "sky_des")
+    private String skyDes;
+
+    @Column(name = "check_date")
+    private Timestamp checkDate;
 }
--- src/main/java/app/weatherapp/domain/RealWeather.java
+++ src/main/java/app/weatherapp/domain/RealWeather.java
@@ -1,4 +1,38 @@
 package app.weatherapp.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+@Table(name = "real_time_weather")
+@Entity
+@Getter
 public class RealWeather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ST_id;
+
+    @JoinColumn(name = "location_id")
+    @ManyToOne
+    private Location location;
+
+    @Column(name = "temperature")
+    private Double temperature;
+
+    @Column(name = "humidity")
+    private Double humidity;
+
+    @Column(name = "rainFall")
+    private Integer rainFall;
+
+    @Column(name = "wind_speed")
+    private Double windSpeed;
+
+    @Column(name = "wind_direction")
+    private Integer windDirection;
+
+    @Column(name = "checkTime")
+    private Timestamp checkTime;
 }
